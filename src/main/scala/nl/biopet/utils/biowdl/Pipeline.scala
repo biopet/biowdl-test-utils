6d6d52e9048064248ed494f40217c529ec3df1fe
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-05-24 22:32:04 +0200

    Adding log and file checks

--- src/main/scala/nl/biopet/utils/biowdl/Pipeline.scala
+++ src/main/scala/nl/biopet/utils/biowdl/Pipeline.scala
@@ -1,14 +1,24 @@
 package nl.biopet.utils.biowdl
 
-import java.io.File
+import java.io.{File, PrintWriter}
 
+import nl.biopet.utils.io.writeLinesToFile
 import nl.biopet.test.BiopetTest
 import nl.biopet.utils.conversions
-import org.testng.annotations.BeforeClass
+import org.testng.annotations.{BeforeClass, DataProvider, Test}
+import play.api.libs.json.Json
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.io.Source
+import scala.sys.process.{Process, ProcessLogger}
+import scala.util.matching.Regex
 
 trait Pipeline extends BiopetTest {
   /** This can be overwritten by the pipeline */
   def inputs: Map[String, String] = Map()
+
+  def logFile: File = new File(outputDir, "log.out")
 
   /** Output dir of pipeline */
   lazy val outputDir =
@@ -19,10 +29,23 @@ trait Pipeline extends BiopetTest {
     val inputsFile = Pipeline.writeInputs(outputDir, inputs)
 
     val cmd: Seq[String] = Seq("java", "-jar", cromwellJar.getAbsolutePath, "run",
-      "-c", cromwellConfig.getAbsolutePath,
-      "-i", inputsFile.getAbsolutePath)
+      "-i", inputsFile.getAbsolutePath) ++ cromwellConfig.toList.flatMap(c => Seq("-c", c.getAbsolutePath))
 
-    //TODO: run cromwell
+    if (!outputDir.exists()) outputDir.mkdirs()
+    if (logFile.exists()) logFile.delete()
+    val future = Future {
+      val writer = new PrintWriter(logFile)
+
+      def writeLine(line: String): Unit = {
+        writer.println(line)
+        writer.flush()
+      }
+
+      val process = Process(cmd, outputDir).run(ProcessLogger(line => writeLine(line)))
+      _exitValue = Some(process.exitValue())
+      writer.close()
+    }
+    Await.result(future, Duration.Inf)
   }
 
   private var _exitValue: Option[Int] = None
@@ -30,14 +53,75 @@ trait Pipeline extends BiopetTest {
   /** exitvalue of the pipeline, if this is -1 the pipeline is not executed yet */
   def exitValue: Option[Int] = _exitValue
 
+  private var mustHaveFiles: List[File] = Nil
+  def addMustHaveFile(path: String*): Unit =
+    mustHaveFiles ::= new File(outputDir, path.mkString(File.separator))
+
+  @DataProvider(name = "must_have_files")
+  def mustHaveFilesProvider: Array[Array[File]] = mustHaveFiles.map(Array(_)).toArray
+
+  @Test(dataProvider = "must_have_files", dependsOnGroups = Array("parseLog"))
+  def testMustHaveFile(file: File): Unit = withClue(s"file: $file") {
+    file should exist
+  }
+
+  private var mustNotHaveFiles: List[File] = Nil
+  def addMustNotHaveFile(path: String*): Unit =
+    mustNotHaveFiles ::= new File(outputDir, path.mkString(File.separator))
+
+  @DataProvider(name = "must_not_have_files")
+  def mustNotHaveFilesProvider: Array[Array[File]] = mustNotHaveFiles.map(Array(_)).toArray
+
+  @Test(dataProvider = "must_not_have_files", dependsOnGroups = Array("parseLog"))
+  def testMustNotHaveFile(file: File): Unit = withClue(s"file: $file") {
+    assert(!file.exists())
+  }
+
+  def addConditionalFile(condition: Boolean, path: String*): Unit = {
+    if (condition) mustHaveFiles ::= new File(outputDir, path.mkString(File.separator))
+    else mustNotHaveFiles ::= new File(outputDir, path.mkString(File.separator))
+  }
+
+  @Test(priority = -1) def exitcode(): Unit = exitValue shouldBe 0
+  @Test def outputDirExist(): Unit = assert(outputDir.exists())
+
+  private var _logLines: List[String] = _
+  def logLines: List[String] = _logLines
+
+  @Test(groups = Array("parseLog"))
+  def logFileExist(): Unit = {
+    assert(logFile.exists())
+    _logLines = Source.fromFile(logFile).getLines().toList
+  }
+
+  private var logMustHave: List[Regex] = Nil
+  def logMustHave(r: Regex): Unit = logMustHave :+= r
+
+  @DataProvider(name = "log_must_have")
+  def logMustHaveProvider: Array[Array[Regex]] = logMustHave.map(Array(_)).toArray
+
+  @Test(dataProvider = "log_must_have", dependsOnGroups = Array("parseLog"))
+  def testLogMustHave(r: Regex): Unit = withClue(s"regex: $r") {
+    assert(logLines.exists(r.findFirstMatchIn(_).isDefined), s"Logfile does not contains: $r")
+  }
+
+  private var logMustNotHave: List[Regex] = Nil
+  def logMustNotHave(r: Regex): Unit = logMustNotHave :+= r
+
+  @DataProvider(name = "log_must_not_have")
+  def logMustNotHaveProvider: Array[Array[Regex]] = logMustNotHave.map(Array(_)).toArray
+
+  @Test(dataProvider = "log_must_not_have", dependsOnGroups = Array("parseLog"))
+  def testLogMustNotHave(r: Regex): Unit = withClue(s"regex: $r") {
+    val i = logLines.indexWhere(r.findFirstMatchIn(_).isDefined)
+    assert(i == -1, s"at line number ${i + 1} in logfile does contains: $r")
+  }
 }
 
 object Pipeline {
   def writeInputs(outputDir: File, inputs: Map[String, String]): File = {
     val outputFile = new File(outputDir, "inputs.json")
-
-    val json = conversions.mapToJson(inputs)
-    //TODO: write inputs
+    writeLinesToFile(outputFile, List(Json.stringify(conversions.mapToJson(inputs))))
 
     outputFile
   }
--- src/main/scala/nl/biopet/utils/biowdl/package.scala
+++ src/main/scala/nl/biopet/utils/biowdl/package.scala
@@ -4,78 +4,39 @@ import java.io.File
 
 package object biowdl {
   lazy val cromwellJar: File = {
-    System.getProperties.getProperty("cromwell.jar") match {
-      case s: String => {
-        val file = new File(s)
-        if (!file.exists())
-          throw new IllegalArgumentException("Cromwell jar '" + file + "' does not exist")
-        file
-      }
-      case _ =>
-        throw new IllegalArgumentException(
-          "No cromwell jar found, please set the 'cromwell.jar' property")
-    }
+    val file = Option(System.getProperties.getProperty("cromwell.jar")).map(new File(_))
+      .getOrElse(throw new IllegalArgumentException("No cromwell jar found, please set the 'cromwell.jar' property"))
+    require(file.exists(), s"Cromwell jar '$file' does not exist")
+    file
   }
 
-  lazy val cromwellConfig: File = {
-    System.getProperties.getProperty("cromwell.config") match {
-      case s: String => {
-        val file = new File(s)
-        if (!file.exists())
-          throw new IllegalArgumentException("Cromwell config '" + file + "' does not exist")
-        file
-      }
-      case _ =>
-        throw new IllegalArgumentException(
-          "No cromwell config found, please set the 'cromwell.config' property")
-    }
+  lazy val cromwellConfig: Option[File] = {
+    val file = Option(System.getProperties.getProperty("cromwell.config")).map(new File(_))
+    file.foreach(f => require(f.exists(), s"Cromwell config '$file' does not exist"))
+    file
   }
 
   lazy val speciesDir: Option[File] = {
-    System.getProperties.getProperty("species.dir") match {
-      case path: String => Some(new File(path))
-      case _ => None
-    }
+    Option(System.getProperties.getProperty("species.dir")).map(new File(_))
   }
 
   lazy val globalOutputDir: File = {
-    System.getProperties.getProperty("biowdl.output_dir") match {
-      case s: String => new File(s)
-      case _ =>
-        throw new IllegalArgumentException(
-          "No output_dir found, please set the 'biowdl.output_dir' property")
-    }
+    Option(System.getProperties.getProperty("biowdl.output_dir")).map(new File(_))
+      .getOrElse(throw new IllegalArgumentException("No output_dir found, please set the 'biowdl.output_dir' property"))
   }
 
+  lazy val functionalTests: Boolean =
+    Option(System.getProperties.getProperty("biowdl.functionalTests")).exists(_ != "false")
 
-  lazy val functionalTests: Boolean = {
-    System.getProperties.getProperty("biowdl.functionalTests") match {
-      case "false" => false
-      case null => false
-      case _ => true
-    }
-  }
-
-  lazy val integrationTests: Boolean = {
-    System.getProperties.getProperty("biowdl.integrationTests") match {
-      case "false" => false
-      case null => true
-      case _ => true
-    }
-  }
+  lazy val integrationTests: Boolean =
+    !Option(System.getProperties.getProperty("biowdl.integrationTests")).contains("false")
 
   lazy val fixtureDir: File = {
-    System.getProperties.getProperty("biowdl.fixture_dir") match {
-      case s: String => {
-        val dir = new File(s)
-        require(dir.exists(), "Fixture directory does not exist: " + s)
-        require(dir.isDirectory, "Fixture directory is not a directory: " + s)
-        dir
-      }
-      case _ =>
-        throw new IllegalArgumentException(
-          "No output_dir found, please set the 'biowdl.fixture_dir' property")
-    }
+    val dir = Option(System.getProperties.getProperty("biowdl.fixture_dir")).map(new File(_))
+      .getOrElse(throw new IllegalArgumentException("No output_dir found, please set the 'biowdl.fixture_dir' property"))
+    require(dir.exists(), s"Fixture directory does not exist: $dir")
+    require(dir.isDirectory, s"Fixture directory is not a directory: $dir")
+    dir
   }
 
   def fixtureFile(paths: String*): File = {
