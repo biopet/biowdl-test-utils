0d4d9330fff1438ad4e9f2793183e9a25751ec38
sa_ci <sa_ci@res-glxy-app01.researchlumc.nl>
2018-06-03 15:48:13 +0200

    updated site

---
+++ develop/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"nl" : [], "nl.biopet" : [], "nl.biopet.utils" : [], "nl.biopet.utils.biowdl" : [{"object" : "nl\/biopet\/utils\/biowdl\/Pipeline$.html", "trait" : "nl\/biopet\/utils\/biowdl\/Pipeline.html", "name" : "nl.biopet.utils.biowdl.Pipeline"}, {"trait" : "nl\/biopet\/utils\/biowdl\/PipelineSuccess.html", "name" : "nl.biopet.utils.biowdl.PipelineSuccess"}]};
