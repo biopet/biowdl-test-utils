719b5e5d8a4a0b73588abb34a8f58447b5dfa45b
ffinfo <pjrvanthof@gmail.com>
2018-08-23 11:53:25 +0200

    Added reference with alt sequences

---
+++ src/main/scala/nl/biopet/utils/biowdl/references/TestReferenceAlt.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package nl.biopet.utils.biowdl.references
+
+import java.io.File
+
+import nl.biopet.utils.biowdl.{fixtureDir, fixtureFile}
+
+/**
+  * Created by pjvanthof on 14/11/15.
+  */
+trait TestReferenceAlt extends Reference {
+  def referenceSpecies = "test"
+  def referenceName = "test_alt"
+  def referenceFasta: File = fixtureFile("reference_alt/reference.fasta")
+
+  override def bwaMemFasta =
+    Some(fixtureFile("reference_alt/bwa/reference.fasta"))
+}
