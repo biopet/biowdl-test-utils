a381cac16ee61a4f8cd7e18913dbd3b16ab56f69
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-08-26 09:39:38 +0200

    Adding tests

--- src/test/scala/nl/biopet/utils/biowdl/PipelineTest.scala
+++ src/test/scala/nl/biopet/utils/biowdl/PipelineTest.scala
@@ -21,10 +21,48 @@
 
 package nl.biopet.utils.biowdl
 
+import java.io.File
+
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
 class PipelineTest extends BiopetTest {
   @Test
-  def testDummy(): Unit = {}
+  def testCreateFile(): Unit = {
+    val outDir = File.createTempFile("test.", ".yml")
+    outDir.delete()
+    outDir.mkdir()
+
+    val pipeline = new Pipeline {
+      override def startFile: File = new File(".")
+      val file = createFile("bla")
+      override def outputDir: File = outDir
+    }
+
+    pipeline.file shouldBe new File(pipeline.outputDir, "bla")
+    pipeline.mustHaveFilesProvider shouldBe Array(
+      Array(new File(pipeline.outputDir, "bla")))
+  }
+
+  @Test
+  def testCreateOptionalFile(): Unit = {
+    val outDir = File.createTempFile("test.", ".yml")
+    outDir.delete()
+    outDir.mkdir()
+
+    val pipeline = new Pipeline {
+      override def startFile: File = new File(".")
+      val file = createOptionalFile(true, "true")
+      val fileNot = createOptionalFile(false, "false")
+      override def outputDir: File = outDir
+    }
+
+    pipeline.file shouldBe Some(new File(pipeline.outputDir, "true"))
+    pipeline.mustHaveFilesProvider shouldBe Array(
+      Array(new File(pipeline.outputDir, "true")))
+    pipeline.fileNot shouldBe None
+    pipeline.mustNotHaveFilesProvider shouldBe Array(
+      Array(new File(pipeline.outputDir, "false")))
+  }
+
 }
