d8a8da47771da59cf15cd56cc609b5531c044f64
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-11-01 16:20:29 +0100

    inputsfile written at beforeclass

--- src/main/scala/nl/biopet/utils/biowdl/Pipeline.scala
+++ src/main/scala/nl/biopet/utils/biowdl/Pipeline.scala
@@ -108,7 +108,7 @@ trait Pipeline extends BiopetTest with Logging {
     } else None
   }
 
-  lazy private val inputsFile: File = Pipeline.writeInputs(outputDir, inputs)
+  lazy private val inputsFile: File = new File(outputDir, "inputs.json")
 
   @BeforeClass
   def run(): Unit = {
@@ -121,7 +121,10 @@ trait Pipeline extends BiopetTest with Logging {
       deleteDirectory(outputDir)
     }
     outputDir.mkdirs()
-    val inputs = inputsFile
+
+    // Write input data to inputsFile
+    writeLinesToFile(inputsFile,
+                     List(Json.stringify(conversions.mapToJson(inputs))))
     runPipeline()
   }
 
@@ -277,14 +280,4 @@ trait Pipeline extends BiopetTest with Logging {
   }
 
   def functionalTest: Boolean = false
-}
-
-object Pipeline {
-  def writeInputs(outputDir: File, inputs: Map[String, Any]): File = {
-    val outputFile = new File(outputDir, "inputs.json")
-    writeLinesToFile(outputFile,
-                     List(Json.stringify(conversions.mapToJson(inputs))))
-
-    outputFile
-  }
 }
